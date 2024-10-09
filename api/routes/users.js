75dd0492fe47493710495ad6517a471591a2a0d7
Oussama <sharp.oussama@gmail.com>
2024-10-09 19:39:39 +0100

    a

--- api/routes/users.js
+++ api/routes/users.js
@@ -60,7 +60,7 @@ router.get("/register/google/callback",
 passport.authenticate('google', { failureRedirect: "/" ,session: false,}),
  async function register_google_callback(req, res){
   console.log('here')
-  res.send('./redirect.html');
+  res.sendFile('./redirect.html');
 });
 
 router.post("/login", async function login_user(req, res) {
